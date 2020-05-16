0db69bed8313d8990597dadd8debb27db70789d7
awen0707 <15040326677@163.com>
2020-05-16 11:34:12 +0800

    11

--- src/views/user/admin.vue
+++ src/views/user/admin.vue
@@ -1,78 +1,94 @@
 <template>
-
-  <el-row class="tac">
-    <el-col :span="12">
-      <el-menu
-        default-active="1-1"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b"
-        mode="vertical"
-        unique-opened
-        :collapse="isCollapse"
-        :collapse-transition="false"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-        @select="handleSelect"
-      ><!-- eslint-disable -->
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </template>
-          <el-menu-item index="2-1">选项2-1</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-    <el-col>
-      <el-button @click="isCollapse = !isCollapse">折叠</el-button>
-    </el-col>
-  </el-row>
+  <div>
+    <el-button
+      class="filter-item"
+      type="primary"
+      text-align="center"
+      icon="el-icon-edit"
+      style="margin-bottom: 5px;margin-top: 5px;margin-left: 20px"
+      @click="handleCreate"
+    >
+      新增管理员
+    </el-button>
+    <el-table
+      :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 100%"
+      align="center"
+    >
+      <el-table-column
+        label="姓名"
+        prop="name"
+      />
+      <el-table-column
+        label="账号"
+        prop="account"
+      />
+      <el-table-column
+        label="联系电话"
+        prop="phone"
+      />
+      <el-table-column
+        align="right"
+      >
+        <template slot="header">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入关键字搜索"
+          />
+        </template>
+        <template slot-scope="scope">
+          <adminedit title="管理员信息" :data="row" align="center">
+            <el-button
+              size="mini"
+            >编辑</el-button>
+          </adminedit>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
-
+<style>
+.filter-item{
+  text-align: center;
+}
+</style>
 <script>
+
+import adminedit from './components/adminedit'
+
 export default {
+  components: { adminedit },
   data() {
     return {
-      isCollapse: false
+      tableData: [{
+        name: '王一',
+        account: '1705',
+        phone: '13888888887'
+      }, {
+        name: '王二',
+        account: '1704',
+        phone: '13888888888'
+      }, {
+        name: '王三',
+        account: '1703',
+        phone: '13888888889'
+      }, {
+        name: '王四',
+        account: '1702',
+        phone: '13888888880'
+      }],
+      search: ''
     }
   },
   methods: {
-    handleSelect(key, keyPath) {
-      console.log('handleSelect', key, keyPath)
-    },
-    handleOpen(key, keyPath) {
-      console.log('handleOpen', key, keyPath)
-    },
-    handleClose(key, keyPath) {
-      console.log('handleClose', key, keyPath)
+    handleDelete(index, row) {
+      console.log(index, row)
     }
   }
 }
---
+++ src/views/user/components/adminedit.vue
@@ -0,0 +1,62 @@
+<template>
+  <span style="padding: 0 10px">
+    <el-dialog :title="title" :visible.sync="visible" :modal="false">
+      <el-form
+        :model="formData"
+        label-position="center"
+        label-width="100px"
+        style="width: 400px; margin-left:50px;"
+      >
+        <el-form-item label="姓名">
+          <el-input v-model="formData.name" disabled />
+        </el-form-item>
+        <el-form-item label="账号">
+          <el-input v-model="formData.account" disabled />
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formData.password" disabled />
+        </el-form-item>
+        <el-form-item label="联系电话">
+          <el-input v-model="formData.phone" disabled />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="visible = false">
+          关闭
+        </el-button>
+      </div>
+    </el-dialog>
+    <span @click="visible = true">
+      <slot />
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+      visible: false,
+      formData: {}
+    }
+  },
+  created() {
+    this.formData = Object.assign({}, this.data)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
